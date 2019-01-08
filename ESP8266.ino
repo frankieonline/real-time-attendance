54e1782a651502c6fa53076e8cf631d6e3f68dc1
frankieonline <frankie.wwl@gmail.com>
2019-01-08 15:16:19 +0800

    Update ESP8266.ino

--- ESP8266.ino
+++ ESP8266.ino
@@ -1,6 +1,6 @@
-#include <SoftwareSerial.h>				/* Call Library */
+#include <SoftwareSerial.h>					/* Call Library */
 
-SoftwareSerial ESP(3, 2);     		    /* Rx, Tx */
+SoftwareSerial ESP(3, 2);     		   			 /* Rx, Tx */
 
 void setup() {
 	Serial.begin(9600);					/* Serial Port, 9600 or 115200 */
@@ -11,7 +11,7 @@ void setup() {
 void loop() {
 	/* If received data from Serial Console, sent to ESP-01 */
 	if (Serial.available()) {
-		ESP.print(Serial.read());		/* If ESP has no response, change "print" to "write"
+		ESP.print(Serial.read());			/* If ESP has no response, change "print" to "write"
 	}
 	
 	/* If received data from ESP-01, sent to Serial Console */
